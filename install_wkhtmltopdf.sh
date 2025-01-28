ff0d7a343e53305a1e6a1c198d277a3fe738eb63
Jerzy Kozlowski <jerzykozlowski123@gmail.com>
2025-01-28 19:51:32 +0100

    add sh

---
+++ install_wkhtmltopdf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Install wkhtmltopdf
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
+dpkg -i wkhtmltox_0.12.6-1.bionic_amd64.deb || apt-get install -f
